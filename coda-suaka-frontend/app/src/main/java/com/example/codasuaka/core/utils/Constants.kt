7a7f79817048a068b1f3575ab85094ff985930f1
Arif Firman Maulana Sidiq <mahes.anjim123@gmail.com>
2026-05-26 16:24:57 +0700

    chore: setup awal backend laravel & arsitel mvvm kotlin

---
+++ coda-suaka-frontend/app/src/main/java/com/example/codasuaka/core/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.codasuaka.core.utils
+
+object Constants {
+    const val BASE_URL = "https://192.168.1.15:8000/api"
+}
---
+++ coda-suaka-frontend/app/src/main/java/com/example/codasuaka/data/local/AppDatabase.kt
@@ -0,0 +1,2 @@
+package com.example.codasuaka.data.local
+
